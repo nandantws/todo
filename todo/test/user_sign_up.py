ab6f78ab0d305b84729ff22c4c653bff3d605568
Gurcharansingh5 <dalbeer.singh@playchallenger.com>
2022-01-05 16:45:34 +0530

    test case for getting todo list

--- todo/test/user_sign_up.py
+++ todo/test/user_sign_up.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APITestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 
@@ -13,7 +13,8 @@ class UserSignUpTestCase(APITestCase):
         cls.username = 'nj'
         cls.first_name = 'nandan'
         cls.last_name = 'jain'
-        cls.user = User.objects.create(username = 'aj',password='test_Pass',email='anne@user.com',first_name = 'anne',last_name = 'jaey')
+        cls.user = User.objects.create(
+            username='aj', password='test_Pass', email='anne@user.com', first_name='anne', last_name='jaey')
 
     def test_if_data_is_correct_then_signup(self):
         # Prepare data
@@ -29,7 +30,8 @@ class UserSignUpTestCase(APITestCase):
 
         # Check status response
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(str(response.data['message']),'User Created Successfully.')
+        self.assertEqual(
+            str(response.data['message']), 'User Created Successfully.')
         self.assertTrue('token' in response.data)
 
         # Check database
@@ -37,15 +39,12 @@ class UserSignUpTestCase(APITestCase):
         new_user = User.objects.get(username=self.username)
         self.assertEqual(new_user.first_name, self.first_name)
         self.assertEqual(new_user.last_name, self.last_name)
-        print(User.objects.all())
-        print('-------------------')
 
 
     def test_if_username_already_exists_dont_signup(self):
-        print(User.objects.all())
         # Prepare data
-        signup_dict ={
-            'username': self.user.username,
+        signup_dict = {
+            'username':self.user.username,
             'password': 'test_Pass',
             'email': 'nandan@user.com',
             'first_name': self.first_name,
@@ -54,14 +53,12 @@ class UserSignUpTestCase(APITestCase):
 
         # Make request
         response = self.client.post(self.signup_url, signup_dict)
-        
+
         # Check status response
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(str(response.data['username'][0]),'A user with that username already exists.')
+        self.assertEqual(
+            str(response.data['username'][0]), 'A user with that username already exists.')
 
         # Check database
-        self.assertEqual(User.objects.filter(username = self.user.username).count(), 1)
-
-
-
-
+        self.assertEqual(User.objects.filter(
+            username=self.user.username).count(), 1)
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,3 @@
-from django.contrib import auth
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 
